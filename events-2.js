01751d3bcb681c98e39ff57090a2cca51ca73067
Tam Byrne <mercadier122@gmail.com>
2019-11-12 14:21:42 +0000

    js update

--- events-2.js
+++ events-2.js
@@ -3,17 +3,17 @@
 function getDogs() {
     fetch('https://dog.ceo/api/breeds/image/random/' + 'number')
     .then(response => response.json())
-    .then(responseJson => console.log(responseJson));
-    showInDom();
+    .then(responseJson => showImage(responseJson));
+    showImage();
 }
 
-function showInDom(responseJson) {
+/*function showInDom(responseJson) {
     console.log(responseJson);
     $('.dog-img').replaceWith(
       `<img src="${responseJson.message}" class="dog-img">`
     )
     $('.dog-results').show();
-  }
+  }*/
 
 function watchForm() {
     $('form').submit(event => {
@@ -23,11 +23,11 @@ function watchForm() {
     });
   }
 
-  /*function showImage() {
+  function showImage() {
     console.log(responseJson);
     $('.dog-img').attr('src', `${responseJson.message}`);
     $('.dog-results').show();
-  }*/
+  }
   
   $(function() {
       console.log('App loaded - submit a number');
